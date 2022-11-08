57b80a36f056fd8c4bb823da1431460cd51a9e2c
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-11-08 11:44:28 +0530

    Employee is Present Or Absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,10 @@
-console.log("Welcome To Employee Wage Problem Statement Using Javascript")
+//UC1-Emplooyee is Present or Absent
+const IS_ABSENT = 0 
+
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if(empCheck == IS_ABSENT) {
+    console.log("Employee is Absent");
+    return;
+} else {
+    console.log("Employee is Present");
+}
